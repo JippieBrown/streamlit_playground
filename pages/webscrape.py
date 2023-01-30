2bc4edac537ad43eafb229d01480cd0688c62790
JippieBrown <haukemaier@gmail.com>
2023-01-30 11:04:03 +0100

    .

---
+++ pages/webscrape.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+import requests
+from bs4 import BeautifulSoup
+
+def search(query):
+    # Suchanfrage an die Google-Suche senden
+    url = "https://www.google.com/search?q=" + query
+    response = requests.get(url)
+
+    # HTML-Antwort parsen
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Erstes Suchergebnis auswählen
+    result = soup.find("div", {"class": "g"})
+
+    # Titel und Link des Suchergebnisses ausgeben
+    print(result.find("h3").text)
+    print(result.find("a")["href"])
+
+# Beispielaufruf
+search("Python programming language")
+
+st.write(search("Python programming language"))
